257aa141ccf164bde5ebb28134fae20c548f0ed4
Vishant <44126013+vishantbhole@users.noreply.github.com>
2025-07-06 11:20:03 -0400

    Update removeDuplicates.py

--- removeDuplicates.py
+++ removeDuplicates.py
@@ -14,3 +14,8 @@ class Solution(object):
                 nums[write] = nums[read]
                 write += 1
         return write
+
+if __name__ == "__main__":
+    obj = Solution()
+    nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
+    print(obj.removeDuplicates(nums))
